a2080d00553ff8cbd2d651d7fc07431b8c5c16d0
paolo215 <paolo_villanueva215@yahoo.com>
2017-06-08 00:03:10 -0400

    Added follow

--- server.py
+++ server.py
@@ -4,6 +4,8 @@ from gevent.wsgi import WSGIServer
 import oauth2
 import config
 import json
+import requests
+
 
 app = Flask(__name__)
 
@@ -22,6 +24,13 @@ def index():
     return render_template("index.html", timelines=timelines)
 
 
+@app.route("/follow/<screen_name>")
+def getFeed(screen_name):
+    timeline = getTimeline(screen_name)
+    return render_template("index.html", timelines={screen_name: timeline})
+
+
+
 # https://dev.twitter.com/oauth/overview/single-user
 def oauth_req(url, key, secret, http_method="GET", post_body="", http_headers=None):
     consumer = oauth2.Consumer(key=CONSUMER_KEY, secret=CONSUMER_SECRET)
@@ -30,11 +39,15 @@ def oauth_req(url, key, secret, http_method="GET", post_body="", http_headers=No
     response, content = client.request(url, method=http_method, body=post_body, headers=http_headers)
     return json.loads(content)
 
+def getTimeline(screen_name):
+    timeline = oauth_req("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=%s&count=10" % screen_name, ACCESS_KEY, ACCESS_SECRET)
+    return timeline 
+
 def main():
     server = WSGIServer(("", 5000,), app)
     for target in targets:
-        timeline = oauth_req("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=realDonaldTrump&count=10", ACCESS_KEY, ACCESS_SECRET)
-        timelines[target] = timeline
+        timelines[target] = getTimeline(target)
+
     server.serve_forever()
 
 
